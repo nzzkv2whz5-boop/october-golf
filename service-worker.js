957e3a2c485cc3fb575c78e1eea39b22eafd29ca
nzzkv2whz5-boop <nzzkv2whz5@privaterelay.appleid.com>
2026-09-23 16:59:31 -0700

    Add compact sticky date picker and expandable calendar

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'october-golf-v2';
+const CACHE = 'october-golf-v3';
 const ASSETS = [
   './', './index.html', './styles.css', './app.js', './data/courses.js',
   './manifest.webmanifest', './icons/icon.svg', './icons/icon-192.png',
